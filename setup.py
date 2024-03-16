06110e065f6f3bf9cba4d81ee53b23baa891da90
aredden <alexander.h.redden@gmail.com>
2024-03-16 17:01:27 -0400

    Small fix

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ check_device_capability_minimum_allowed()
 
 setup(
     name="torch_bnb_fp4",
-    version="0.0.8",
+    version="0.0.9",
     packages=find_packages(
         exclude=[
             "csrc",
@@ -64,7 +64,7 @@ setup(
             ".misc/*",
         ]
     ),
-    requires=["bitsandbytes", "prettytable", "accelerate"],
+    requires=["bitsandbytes<0.43", "prettytable", "accelerate"],
     ext_modules=[
         CUDAExtension(
             name="torch_bnb_fp4_ext",
